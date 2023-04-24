da7690fc0b76619ac23a02798a216d1cc9b20c38
Гулафзо <102953657+Gulafzo@users.noreply.github.com>
2023-04-24 10:09:57 +0300

    Add files via upload

---
+++ Database1/Database1/Program.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Console_Data_Base
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string SelectSQLcommand = @"SELECT c.Name AS ""Person Name"", cc.ClassName AS ""Class Name"" FROM Characters AS c, CharactersClass AS cc WHERE c.CharacterClassID = cc.ID;";
+
+            MySqlConnection connection = null;
+            MySqlDataReader reader = null;
+            MySqlConnectionStringBuilder mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder()
+            {
+                Server = "db4free.net",
+                Database = "isoevagulafzo",
+                UserID = "gulafzo",
+                Password = "_TZWsQ836d@_wAf",
+                OldGuids = true
+            };
+
+            //connection = new SqlConnection();
+            //connection.ConnectionString = @"Data Source=db4free.net:3306 ; Initial Catalog=first_test_bd; User ID=anonim; Password=anonim228";
+
+            try
+            {
+
+
+                connection = new MySqlConnection(mySqlConnectionStringBuilder.ToString());
+                await connection.OpenAsync();
+
+                //SqlCommand cmd = new SqlCommand();
+                //cmd.Connection = connection;
+                //cmd.CommandText = SelectSQLcommand;
+
+                MySqlCommand cmd = new MySqlCommand(SelectSQLcommand, connection);
+
+                //cmd.ExecuteNonQuery();
+
+                reader = (MySqlDataReader)await cmd.ExecuteReaderAsync();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0] + " " + reader[1]);
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    await connection.CloseAsync();
+                }
+                if (reader != null)
+                {
+                    await reader.CloseAsync();
+                }
+            }
+        }
+    }
+}
